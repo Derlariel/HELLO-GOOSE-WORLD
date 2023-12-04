e7c57d6a32e1d716c97460a0413eeb16147d493c
KONGPHOB KONGSAN <142288866+Kpsax04@users.noreply.github.com>
2023-12-04 17:49:48 +0700

    Add files via upload

---
+++ component-main/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <div className=" bg-purple-950">
+        <h1 className=" flex justify-center items-center text-center text-white">
+          NAVBAR
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
