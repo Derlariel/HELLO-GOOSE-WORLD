86b9ebb9878ec9be99328a479b2c69b8cc1d2fba
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2023-12-05 19:50:38 +0700

    Reset

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './css/App.css'
-import Nav from './Nav'
 
 const App = () => {
   return (
     <>
-      <Nav />
       <div className="text-red-500">Hello World</div>
     </>
   )
