dbecd2f09626e534a22ea3c14ccf6ff914b331e7
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2023-12-08 20:09:55 +0700

    Update Register

--- src/components-login/RegisterButton.tsx
+++ src/components-login/RegisterButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 interface RegProps {
     text: string;
@@ -8,8 +9,9 @@ const RegButton = (props: RegProps) => {
     const {text} = props
     return (
         <>
-        <button className="rounded-2xl  divide-solid h-16 w-80  text-2xl font-semibold bg-[#dd9973] border border-black"><a href="">{text}</a></button>
-        
+        <button className="rounded-2xl  divide-solid h-16 w-80  text-2xl font-semibold bg-[#dd9973] border border-black">
+            <Link to="/register">{text}</Link>
+        </button>
         </>
     )
 }
